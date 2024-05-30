a959fd6b47b70b0e6233bfc5e6abdc530f4ccc8c
Illutas <107239220+Illutas@users.noreply.github.com>
2024-05-30 15:30:49 +0200

    chore: Update toolbar component with sidebar functionality

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+
 
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatIconModule, MatToolbarModule, MatSidenavModule],
+  imports: [MatIconModule, MatToolbarModule, MatSidenavModule, MatButtonModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
 
+  showFiller = false;
 }
